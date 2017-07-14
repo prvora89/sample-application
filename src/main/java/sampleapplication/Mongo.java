a9cf1611636d334247b2c1c51e6e03b7cc96cea6
Jarrett Ruppert <jruppert@student.neumont.edu>
2017-07-14 14:22:04 -0600

    Connected Java to MongoDB

--- src/main/java/sampleapplication/Mongo.java
+++ src/main/java/sampleapplication/Mongo.java
@@ -1,5 +1,16 @@
 package sampleapplication;
 
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
 public class Mongo {
+	static MongoClient mc = new MongoClient("localhost");
+	static MongoDatabase md = mc.getDatabase("test");
+	static MongoCollection col = md.getCollection("restaurants");
 	
+	public static void Main(String[] args){
+		
+	}
 }
+	
