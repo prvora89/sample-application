f162158c81699cdbc2d57274d23f195a54d69f1b
jdbowd01 <jordon.bowden@gmail.com>
2017-07-14 14:19:20 -0600

    Connected java to our mongoDB

--- src/main/java/sampleapplication/Mongo.java
+++ src/main/java/sampleapplication/Mongo.java
@@ -1,5 +1,20 @@
 package sampleapplication;
 
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
 public class Mongo {
+	static MongoClient mc = new MongoClient("localhost");
+	static MongoDatabase db = mc.getDatabase("test");
+	static MongoCollection col = db.getCollection("restaurants");
 	
+	public static void main(String[] args) {
+		System.out.println("Collection: " + col);
+		
+	}
+	
+	public void Insert() {
+		
+	}
 }
