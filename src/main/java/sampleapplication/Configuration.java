4a486c4b9d94eac52c9746a2c52729437e56bd92
Jaden-Giordano <thejadenjack@gmail.com>
2017-07-14 13:49:05 -0600

    Configuration File

--- src/main/java/sampleapplication/Configuration.java
+++ src/main/java/sampleapplication/Configuration.java
@@ -1,24 +1,39 @@
 package sampleapplication;
 
+import com.google.gson.Gson;
+
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Configuration {
 
-    private class ConfigContainer {
-        public String mongoHost;
-
-        public ConfigContainer(String mongoHost) {
-            this.mongoHost = mongoHost;
-        }
-    }
+    public String mongoHost;
 
     public Configuration(String configPath) {
         File configFile = new File(configPath);
 
         if (!configFile.exists()) {
             System.out.println("No configuration file found.");
+            return;
         }
 
+        try (FileReader fr = new FileReader(configFile)) {
+            try (BufferedReader br = new BufferedReader(fr)) {
+                StringBuilder data = new StringBuilder();
+                while (br.ready()) {
+                    data.append(br.readLine());
+                }
+                Gson gson = new Gson();
+                this.mongoHost = gson.fromJson(data.toString(), Configuration.class).mongoHost;
+            }
+        } catch (IOException ignored) {
+        }
+    }
+
+    public String getHost() {
+        return this.mongoHost;
     }
 
 }
